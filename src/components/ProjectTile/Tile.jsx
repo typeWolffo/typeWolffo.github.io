66b96e0b63e48c728d7eeb5153ab981fc26a49b8
typeWolffo <jwilk436@gmail.com>
2021-05-09 23:36:26 +0200

    project tile

---
+++ src/components/ProjectTile/Tile.jsx
@@ -0,0 +1,62 @@
+import React, {Component} from "react";
+import {makeStyles, withStyles} from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+class Tile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bg: '',
+            title: '',
+            desc: '',
+        }
+    }
+
+    render() {
+        const {classes} = this.props;
+
+        return (
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image={this.state.bg}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {this.state.title}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {this.state.desc}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                {/*<CardActions>*/}
+                {/*    <Button size="small" color="primary">*/}
+                {/*        Share*/}
+                {/*    </Button>*/}
+                {/*    <Button size="small" color="primary">*/}
+                {/*        Learn More*/}
+                {/*    </Button>*/}
+                {/*</CardActions>*/}
+            </Card>
+        );
+    }
+}
+
+export default withStyles(useStyles)(Tile);
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
-
+import Tile from "../ProjectTile/Tile";
 class Projects extends React.Component {
     render() {
         return (
+            <div>
             <h1>Projects</h1>
+            <Tile {...this.setState({title: 'test'})}/>
+            </div>
         )
     }
 }
